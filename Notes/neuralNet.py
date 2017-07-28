0404de0eeb5f0544fe4d2644621797c8168fe0f3
Michael Huy Tran <michaelhuytran@gmail.com>
2017-07-28 10:28:25 -0700

    [Feature] - Add First Neural Network exercise - Displays initial random weights and a consistent new synaptic weights given an input and an output

--- Notes/neuralNet.py
+++ Notes/neuralNet.py
@@ -1,6 +1,64 @@
-if __name__ = '__main__':
+from numpy import exp, array, random, dot
 
-    #initialise a single neuron neural network
+class NeuralNetwork():
+    def __init__(self):
+        # Seed the random number generator, so it generates the same numbers
+        # every time the program runs
+        random.seed(1)
+
+        # we model a single neuron with 3 input connections and 1 output connection.
+        # we assign random weights to a 3 x 1 matrix, with values in the range -1 to 1
+        # and mean 0
+        self.synaptic_weights = 2 * random.random((3,1)) -1
+
+    # the sigmoid function which describes an s shaped curve
+    # we pass the weighted sum of the inputs through this function
+    # to normalize them between 0 and 1
+
+    def __sigmoid(self, x):
+        return 1 / (1 + exp(-x))
+
+    # gradient of the sigmoid curve
+    def __sigmoid_derivative(self, x):
+        return x * (1-x)
+
+    def train(self, training_set_inputs, training_set_outputs, number_of_training_iterations):
+        # pass the training set thorough our neural network
+        output = self.predict(training_set_inputs)
+
+        # calculate the error
+        error = training_set_outputs - output
+
+        # multiply the error by the input ad again by the gradient of sigmoid curve
+        adjustment = dot(training_set_inputs.T, error * self.__sigmoid_derivative(output))
+
+        self.synaptic_weights += adjustment
+
+    def predict(self, input):
+        # pass inputs through our neural network (your single neuron)
+        return self.__sigmoid(dot(input, self.synaptic_weights))
+
+
+if __name__ == '__main__':
+
+    # initialise a single neuron neural network
     neural_network = NeuralNetwork()
 
-    print ''
+    print 'Random starting synaptic weights:'
+    print neural_network.synaptic_weights
+
+    # The training set. We have 4 examples, each consisting of 3 input values
+    # and 1 out values.
+    training_set_inputs = array([[0, 0, 1], [1, 1, 1], [1, 0, 1], [0, 1, 1]])
+    training_set_outputs = array([[0, 1, 1, 0]]).T
+
+    # train the neural network using a training set.
+    # Do it 10,000 times and make small adjustments each time
+    neural_network.train(training_set_inputs, training_set_outputs, 10000)
+
+    print 'New synaptic weights after training:'
+    print neural_network.synaptic_weights
+
+    # Test the neural network
+    print 'predicting'
+    print neural_network.predict(array([1, 0, 0]))
